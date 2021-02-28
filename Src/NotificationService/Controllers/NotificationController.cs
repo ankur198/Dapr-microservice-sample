3a984627b3a0477a3cf21bf65c899bd03c7bf384
ankur198 <ankur.nigam198@gmail.com>
2021-02-28 19:55:42 +0000

    notification service created

---
+++ Src/NotificationService/Controllers/NotificationController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NotificationService.Models;
+
+namespace NotificationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly ILogger<NotificationController> _logger;
+        public const string StoreName = "statestore";
+        public const string PubSubName = "pubsub";
+
+
+        public NotificationController(ILogger<NotificationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<List<Notification>> GetNotifications(int userId, [FromServices] DaprClient client)
+        {
+            var notifications = await client.GetStateAsync<List<Notification>>(StoreName, $"user-{userId}");
+            return notifications;
+        }
+
+        [Topic(PubSubName, "newuser")]
+        [HttpPost("newuser")]
+        public async Task<string> NewUserCreatedAsync(User user, [FromServices] DaprClient client)
+        {
+            _logger.LogInformation("New User Created Event");
+            await AddNotificationToUser(user.id, user, "newuser", client);
+            return $"Hello {user.name}";
+        }
+
+
+        private static async Task AddNotificationToUser(int userId, object payload, string type, DaprClient client)
+        {
+            var stateUser = await client.GetStateEntryAsync<List<Notification>>(StoreName, $"user-{userId}");
+            stateUser.Value ??= new();
+            stateUser.Value.Add(new Notification(payload, type));
+            await stateUser.SaveAsync();
+        }
+    }
+}
---
+++ Src/NotificationService/Models/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NotificationService.Models
+{
+    public record User(int id, string name);
+
+    public record Notification
+    {
+        public DateTime Timestamp { get; }
+        public object Payload { get; }
+        public string Type { get; }
+
+        public Notification(object payload, string type)
+        {
+            Timestamp = DateTime.Now;
+            Payload = payload;
+            Type = type;
+        }
+    }
+}
